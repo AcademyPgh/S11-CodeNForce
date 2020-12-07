fb57e4c8ac34ff618f841710623cd9e45c844181
Amy Check <amycheck8715@gmail.com>
2020-12-07 11:30:03 -0500

    branch checkout and bar is active feat

--- BlogThing/Controllers/HomeController.cs
+++ BlogThing/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using BlogThing.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -35,5 +36,7 @@ namespace BlogThing.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        
     }
 }
